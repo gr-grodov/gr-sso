575e6359e9a4077a981a0b399d483748fc89e2ad
gr-grodov <makster2323@gmail.com>
2026-06-19 16:23:46 +0500

    init

---
+++ src/main/java/gr/grodov/grsso/GrSsoApplication.java
@@ -0,0 +1,13 @@
+package gr.grodov.grsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrSsoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gr/grodov/grsso/GrSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.grodov.grsso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
